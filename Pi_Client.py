7d3a0da47fb619d8ccf9675c11907ebab3e5c4f8
Boom244 <bolumsba@gmail.com>
2024-03-01 11:08:00 -0800

    Update Pi_Client.py

--- Pi_Client.py
+++ Pi_Client.py
@@ -31,7 +31,7 @@ else:
     PORT = str(PORT)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(HOST,PORT)
+    s.bind((HOST,PORT))
     s.listen()
     conn,addr = s.accept()
     with conn:
