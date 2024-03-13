e37d6d405c7e7430c3d1dbaf2b7f6c94f4085ed1
Boom244 <bolumsba@gmail.com>
2024-03-13 14:02:15 -0700

    Update Pi_Client.py

--- Pi_Client.py
+++ Pi_Client.py
@@ -9,21 +9,24 @@ import asyncio
 import time
 import subprocess
 from threading import Thread
-left = False
-right = False
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+#GPIO setup
 GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 
+#MediaMTX subprocess
 def MTX_Setup():
 	subprocess.run(["mediamtx"])
 
 Thread(target=MTX_Setup).start()
 
 
+#Websockets handler function
 async def process_input(websocket):
-	global bool
 	while True:
 		async for msg in websocket:
 			highbuffer = []
@@ -37,13 +40,19 @@ async def process_input(websocket):
 			elif msg == "RIGHT":
 				lowbuffer.append(27)
 				highbuffer.append(17)
+			elif msg == "BACKWARDS":
+				highbuffer.append(22)
+				highbuffer.append(23)
+				lowbuffer.append(17)
+				lowbuffer.append(27)
 			elif msg == "STOP":
 				highbuffer.append(17)
-				highbuffer.append(27)
+				lowbuffer.append(27)
 			print("Activated: " + str(highbuffer))
 			print("Deactivated: " + str(lowbuffer))
 			GPIO.output(highbuffer,GPIO.HIGH)
 			GPIO.output(lowbuffer, GPIO.LOW)
+
 loop = asyncio.get_event_loop()
 server = serve(process_input, '10.84.3.157', 5446)
 loop.run_until_complete(asyncio.gather(server))
